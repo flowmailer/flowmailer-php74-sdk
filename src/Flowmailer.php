d00066e81982e7012ddde765f749f9496d03bf5d
Reyo Stallenberg <reyo@flowmailer.com>
2022-05-23 15:01:43 +0000

    PHP 7.4: Merge branch 'nulllogger' into 'main'

--- src/Flowmailer.php
+++ src/Flowmailer.php
@@ -34,6 +34,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriFactoryInterface;
 use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Psr\SimpleCache\CacheInterface;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -112,6 +113,8 @@ class Flowmailer extends Endpoints
         $this->cache = $cache;
         $this->innerHttpClient = $innerHttpClient;
         $this->innerAuthClient = $innerAuthClient;
+        $this->logger = $this->logger ?? new NullLogger();
+
         $this->accountId    = $options->getAccountId();
         $this->clientId     = $options->getClientId();
         $this->clientSecret = $options->getClientSecret();
@@ -178,7 +181,7 @@ class Flowmailer extends Endpoints
 
     public function setLogger(LoggerInterface $logger = null): self
     {
-        $this->logger = $logger;
+        $this->logger = $logger ?? new NullLogger();
 
         return $this;
     }
