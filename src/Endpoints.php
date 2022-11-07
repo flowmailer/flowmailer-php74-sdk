01c5d23c43cf824947f85e2a3602e8e5e53ecd98
Reyo Stallenberg <reyo@flowmailer.com>
2022-11-07 15:00:40 +0000

    PHP 7.4: Merge branch 'mixed-body' into 'main'

--- src/Endpoints.php
+++ src/Endpoints.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace Flowmailer\API;
 
+use Flowmailer\API\Collection\AccountUserCollection;
 use Flowmailer\API\Collection\MessageCollection;
 use Flowmailer\API\Collection\MessageEventCollection;
 use Flowmailer\API\Model\Account;
@@ -62,6 +63,7 @@ abstract class Endpoints
      * @param $clientSecret The API client secret provided by Flowmailer
      * @param $grantType    must be `client_credentials`
      * @param $scope        Must be absent or `api`
+     *
      * @codeCoverageIgnore
      */
     public function createRequestForCreateOAuthToken(
@@ -126,6 +128,7 @@ abstract class Endpoints
      * @param array          $sourceIds      Filter results on message source ID
      * @param bool           $addmessagetags Message tags will be included with each event if this parameter is true
      * @param string         $sortorder
+     *
      * @codeCoverageIgnore
      */
     public function createRequestForGetMessageEvents(
@@ -185,6 +188,7 @@ abstract class Endpoints
      * @param bool           $addtags
      * @param string         $sortfield     Sort by INSERTED or SUBMITTED (default INSERTED)
      * @param string         $sortorder
+     *
      * @codeCoverageIgnore
      */
     public function createRequestForGetMessages(
@@ -270,6 +274,7 @@ abstract class Endpoints
      *
      * @param      $messageId Message ID
      * @param bool $addtags
+     *
      * @codeCoverageIgnore
      */
     public function createRequestForGetMessage($messageId, ?bool $addtags = false): RequestInterface
@@ -292,6 +297,27 @@ abstract class Endpoints
         $response = $this->handleResponse($this->getResponse($request), (string) $request->getBody(), $request->getMethod());
 
         return $this->serializer->deserialize($response, Message::class, 'json');
+    }
+
+    /**
+     * Create the RequestInterface for getUsers.
+     *
+     * @codeCoverageIgnore
+     */
+    public function createRequestForGetUsers(): RequestInterface
+    {
+        return $this->createRequest('GET', sprintf('/%1$s/users', $this->getOptions()->getAccountId()), null, [], [], []);
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getUsers(): AccountUserCollection
+    {
+        $request  = $this->createRequestForGetUsers();
+        $response = $this->handleResponse($this->getResponse($request), (string) $request->getBody(), $request->getMethod());
+
+        return $this->serializer->deserialize($response, AccountUserCollection::class, 'json');
     }
 
     /**
