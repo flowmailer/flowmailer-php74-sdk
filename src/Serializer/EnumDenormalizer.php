b6ac7496680c0fe7085cfd0ccdd4e04b38f015d9
Reyo Stallenberg <reyo@flowmailer.com>
2023-10-16 14:54:56 +0000

    PHP 7.4: Merge branch 'fix-for-getting-enum-values' into 'main'

---
+++ src/Serializer/EnumDenormalizer.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Flowmailer PHP SDK package.
+ * Copyright (c) 2021 Flowmailer BV
+ */
+
+namespace Flowmailer\API\Serializer;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class EnumDenormalizer implements DenormalizerInterface, DenormalizerAwareInterface
+{
+    use DenormalizerAwareTrait;
+
+    /**
+     * @return mixed
+     */
+    public function denormalize($data, string $type, ?string $format = null, array $context = [])
+    {
+        return $data;
+    }
+
+    public function supportsDenormalization($data, string $type, ?string $format = null, array $context = []): bool
+    {
+        $namespaceParts = explode('\\', $type);
+
+        array_pop($namespaceParts);
+
+        return implode('\\', $namespaceParts) === 'Flowmailer\API\Enum';
+    }
+}
